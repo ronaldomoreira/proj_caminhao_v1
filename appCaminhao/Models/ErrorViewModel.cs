f4117ad2b8e0c0b758c0d21f061e555a319aa091
ronal <ronal@DESKTOP-PI83SBO>
2022-02-25 12:21:02 -0300

    Add project files.

---
+++ appCaminhao/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace appCaminhao.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
